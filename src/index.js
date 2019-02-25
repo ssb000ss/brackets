61a8f61f5237780b899e4b5e7ba03cdc3f436af6
ssb000ss <ssb000ss@gmail.com>
2019-02-26 00:14:05 +0600

    task was soluted

--- src/index.js
+++ src/index.js
@@ -1,3 +1,52 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let opening = {}, closing = {}, count = {}, theSame = {};
+    let stack = [];
+
+    bracketsConfig.forEach((item) => {
+        if (item[0] === item[1]) {
+            theSame[item[0]] = true;
+            opening[`${item[0]}+`] = `${item[0]}-`;
+            closing[`${item[1]}-`] = `${item[0]}+`;
+            count[`${item[0]}+`] = 0;
+            count[`${item[1]}-`] = 0;
+        } else {
+            opening[`${item[0]}`] = item[1];
+            closing[`${item[1]}`] = item[0];
+            count[`${item[0]}`] = 0;
+            count[`${item[1]}`] = 0;
+        }
+    });
+
+    let arr = str.split('').map((i) => {
+        let temp;
+        if (theSame[i] === undefined) {
+            temp = i;
+        } else if (theSame[i]) {
+            temp = `${i}+`;
+            theSame[i] = false;
+        } else {
+            temp = `${i}-`;
+            theSame[i] = true;
+        }
+        return temp;
+    });
+
+
+    arr.forEach((currentValue) => {
+        count[currentValue] = +count[currentValue] + 1;
+        if (opening[currentValue]) stack.push(currentValue)
+        else {
+            if (stack.length === 0 || stack[stack.length - 1] !== closing[currentValue]) return false;
+            let br = stack.pop();
+            currentValue = opening[br];
+        }
+    });
+
+    for (let key in opening) {
+        let a = count[key];
+        let b = count[opening[key]];
+        if (a !== b) return false;
+    }
+
+    return stack.length <= 0;
 }
